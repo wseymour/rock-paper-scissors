574c361e063e672a5822bc5cc487fd950b51bab3
Will Seymour <william.seymour.73@gmail.com>
2021-06-23 23:43:52 -0500

    defined computerPlay and object to hold value->result constants

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,16 @@
-console.log("Script Working.");
+const plays = {
+    1: "rock",
+    2: "paper",
+    3: "scissors"
+}
+
+function computerPlay() {
+    let generatedNum = Math.floor(Math.random() * 3) + 1;
+
+    return plays[generatedNum]
+
+    }
+
+// function rockPaperScissors(computerSelection, playerSelectiuon) {
+//     if (computerSelection == 
+// }
