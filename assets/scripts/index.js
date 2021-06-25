62559071311cf1b92b9acbdb0486a13db117dcc8
Will Seymour <william.seymour.73@gmail.com>
2021-06-25 00:14:29 -0500

    implemented round logic, refined computer guess, separated guess number conversion. Need to get prompt working.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,16 +1,42 @@
 const plays = {
-    1: "rock",
-    2: "paper",
-    3: "scissors"
+    "Rock": 0,
+    "Paper": 1,
+    "Scissors": 2
+}
+
+function convertGuess(guess) {
+    if (guess === 0) {
+        return "Rock";
+    } else if (guess === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
 }
 
 function computerPlay() {
-    let generatedNum = Math.floor(Math.random() * 3) + 1;
-
-    return plays[generatedNum]
-
+    let generatedNum = Math.floor(Math.random() * 3);
+    
+    return convertGuess(generatedNum);
+}
+  
+function rockPaperScissors(playerSelection, computerSelection) {
+    if (plays[playerSelection] == plays[computerSelection]) {
+        return "It's a Draw!";
+    } else if ((plays[playerSelection] + 1) % 3 === plays[computerSelection]) {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`
+    } else {
+        return `You win! ${playerSelection} beats ${computerSelection}.`
     }
+}
 
-// function rockPaperScissors(computerSelection, playerSelectiuon) {
-//     if (computerSelection == 
-// }
+function game(){
+    playerWins = 0;
+    compWins = 0;
+
+    for (i = 0; i < 5; i++) {
+        let guess = prompt("Choose 'Rock', 'Paper', or 'Scissors': ");
+
+        rockPaperScissors()
+    }
+}
